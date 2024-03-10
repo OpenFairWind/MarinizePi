6cbb32ce2499809bb679e3b6cb6a4d1a1d4ff242
Raffaele Montella <raffaele.montella@uniparthenope.it>
2024-03-10 23:48:16 +0100

    Added xcompmgr to the packages to install

--- marinizepi.sh
+++ marinizepi.sh
@@ -98,11 +98,9 @@ ignore_broadcast_ssid=0
 EOF
 
 # Setup user interface
-apt install xserver-xorg raspberrypi-ui-mods -y
+apt install xserver-xorg raspberrypi-ui-mods xcompmgr -y
 systemctl set-default graphical.target
 ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
-#rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
-#sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/#autologin-user=/"
 
 # Install and enable vnc server
 apt install realvnc-vnc-server -y
