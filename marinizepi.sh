28addf34d8aa86aba78a2d26047d66189437fdc7
Raffaele Montella <raffaele.montella@uniparthenope.it>
2024-04-05 14:32:45 +0200

    Update marinizepi.sh

--- marinizepi.sh
+++ marinizepi.sh
@@ -230,5 +230,5 @@ apt install chromium-browser -y
 apt install piclone -y
 
 # Install the QT6 runtime
-sudo apt install libnss-mdns avahi-utils libavahi-compat-libdnssd libqt6websockets6 libqt6webenginewidgets6 libqt6webenginecore6 libqt6positioningquick6 libqt6widgets6 libqt6network6 libqt6gui6 libqt6core6 libqt6quickwidgets6 libqt6quickwidgets6 libqt6webchannel6 libqt6qml6 libqt6dbus6 libqt6qmlmodels6 libqt6opengl6 libqt6virtualkeyboard6 qt6-virtualkeyboard-plugin -y
+apt install libnss-mdns avahi-utils libavahi-compat-libdnssd-dev libqt6websockets6 libqt6webenginewidgets6 libqt6webenginecore6 libqt6positioningquick6 libqt6widgets6 libqt6network6 libqt6gui6 libqt6core6 libqt6quickwidgets6 libqt6quickwidgets6 libqt6webchannel6 libqt6qml6 libqt6dbus6 libqt6qmlmodels6 libqt6opengl6 libqt6virtualkeyboard6 qt6-virtualkeyboard-plugin -y
 
